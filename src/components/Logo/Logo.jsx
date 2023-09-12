e6d280e737e82e6addcfcd054106f797b72b7c10
Alitodaroj <aroj34@yahoo.com>
2023-09-11 22:36:17 -0400

    delete item added

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import './Logo.css';
 export default function Logo() {
   return (
     <div className="Logo">
-      <div>Dulce Kajey Cakes</div>
+      <img src=".logo/images/dulce-kajey-cakes.png" alt="company logo"/>
     </div>
   );
 }
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 import * as ordersAPI from '../../utilities/orders-api';
 import './NewOrderPage.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../components/Logo/Logo';
 import MenuList from '../../components/MenuList/MenuList';
 import CategoryList from '../../components/CategoryList/CategoryList';
@@ -14,6 +14,7 @@ export default function NewOrderPage({ user, setUser }) {
     const [activeCat, setActiveCat] = useState([]);
     const [cart, setCart] = useState([null]);
     const categoriesRef = useRef([]);
+    const navigate = useNavigate;
 
     useEffect(function() {
       async function getItems() {
@@ -37,6 +38,15 @@ async function handleAddToOrder(itemId) {
    // 2. Update the cart state with the updated cart received from the server
   setCart(updatedCart);
 };
+async function handleChangeQty(itemId, newQty) {
+  const updatedCart = await ordersAPI.setItemQtyInCart(itemId, newQty);
+  setCart(updatedCart);
+}
+
+async function handleCheckout() {
+  await ordersAPI.checkout();
+  navigate('/orders');
+}
     return (
       <main className="NewOrderPage">
         <aside>
@@ -53,7 +63,10 @@ async function handleAddToOrder(itemId) {
       menuItems={menuItems.filter(item => item.category.name === activeCat)}
       handleAddToOrder={handleAddToOrder}
     />
-  <OrderDetail order ={cart}/>
+  <OrderDetail 
+  order ={cart}
+  handleChangeQty={handleChangeQty}
+  handleCheckout={handleCheckout}/>
 </main>
     )
   }
